c639528dc05d65ef4e9d967aa8d7dd75981cb0a6
topher-manubay <cjmanubay@avinya.ph>
2025-08-13 13:42:00 +0800

    initial commit

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App.tsx";
+import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <MantineProvider
+      defaultColorScheme="light"
+      theme={{
+        fontFamily: 'Poppins, "Century Gothic", Arial, Quicksand, sans-serif',
+      }}
+    >
+      <Notifications />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </MantineProvider>
+  </React.StrictMode>
+);
---
+++ frontend/src/router/AppRoutes.tsx
@@ -0,0 +1,28 @@
+// import { Routes, Route } from "react-router-dom";
+// import Layout from "../layout/Layout";
+// import Home from "../pages/Home.jsx";
+// import Login from "../pages/Login.jsx";
+
+export default function AppRoutes() {
+  return (
+    <></>
+    // <Routes>
+    //   <Route
+    //     path="/"
+    //     element={
+    //       <Layout>
+    //         <Home />
+    //       </Layout>
+    //     }
+    //   />
+    //   <Route
+    //     path="/login"
+    //     element={
+    //       <Layout>
+    //         <Login />
+    //       </Layout>
+    //     }
+    //   />
+    // </Routes>
+  );
+}
