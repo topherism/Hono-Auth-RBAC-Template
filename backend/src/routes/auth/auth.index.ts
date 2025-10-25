b84bcc1fffc0c076e7194de6f300f28422ff66cb
topherism <christophermanubay04@gmail.com>
2025-10-26 02:34:23 +0800

    fix: remove logout authentication middleware

--- backend/src/routes/auth/auth.index.ts
+++ backend/src/routes/auth/auth.index.ts
@@ -15,7 +15,6 @@ const router = createRouter();
 router.use(routes.login.path, ipRateLimiter);
 router.openapi(routes.login, handlers.login);
 
-router.use("/auth/logout/*", authenticationMiddleware);
 router.openapi(routes.logout, handlers.logout);
 router.openapi(routes.logout_all, handlers.logout_all);
 
