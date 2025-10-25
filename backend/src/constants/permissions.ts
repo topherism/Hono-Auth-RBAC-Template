3c893704cbec378c901179372b2164084e3f63fd
topherism <christophermanubay04@gmail.com>
2025-10-26 02:58:44 +0800

    fix: correct casing of PATCH_USER_ROLE permission constant

--- backend/src/constants/permissions.ts
+++ backend/src/constants/permissions.ts
@@ -4,7 +4,7 @@ export const PERMISSIONS = {
   DELETE_USER: "delete_user",
   VIEW_USER: "view_user",
   VIEW_ONE_USER: "view_one_user",
-  PATCH_USER_ROLE: "PATCH_user_role",
+  PATCH_USER_ROLE: "patch_user_role",
   VIEW_ROLE_PERMISSIONS: "view_role_permissions",
   PATCH_ROLE_PERMISSIONS: "patch_role_permissions",
   VIEW_USER_ROLE_PERMISSIONS: "view_user_role_permissions",
